e55cfd94edc09dca14a4621c7bce9429a74a2f41
SPECodeClub <59796951+SPECodeClub@users.noreply.github.com>
2020-01-18 18:10:50 +0000

    Update lesson

--- main.js
+++ main.js
@@ -170,3 +170,44 @@ function checkhowtousegithub(){
 	document.getElementById("number_correct").innerHTML = "You got " + correct + " out of 3 correct.";
 	document.getElementById("picture").src = pictures[score];
 	}
+
+function checkscratchbasics(){
+
+	var question1 = document.quiz.question1.value;
+	var question2 = document.quiz.question2.value;
+	var question3 = document.quiz.question3.value;
+	var correct = 0;
+
+
+	if (question1 == "Lifelong Kindergarten Group") {
+		correct++;
+}
+	if (question2 == "The stage") {
+		correct++;
+}	
+	if (question3 == "Backdrop") {
+		correct++;
+	}
+	
+	var pictures = ["../../../img/win.gif", "../../../img/meh.gif", "../../../img/lose.gif"];
+	var messages = ["Excellent job! Keep it up!", "Good work! You can have another go and try to get a higher score.", "Nice try! Why not have another go?"];
+	var score;
+
+	if (correct == 0) {
+		score = 2;
+	}
+
+	if (correct > 0 && correct < 3) {
+		score = 1;
+	}
+
+	if (correct == 3) {
+		score = 0;
+	}
+
+	document.getElementById("after_submit").style.visibility = "visible";
+
+	document.getElementById("message").innerHTML = messages[score];
+	document.getElementById("number_correct").innerHTML = "You got " + correct + " out of 3 correct.";
+	document.getElementById("picture").src = pictures[score];
+	}
